470e79d7131036d5bc0093490842e63f1d60b768
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2025-08-15 15:25:34 +0200

    refactor(platform): extract userRepository and userController variables

--- src/platform/api/src/infrastructure/routes/UserRoutes.ts
+++ src/platform/api/src/infrastructure/routes/UserRoutes.ts
@@ -2,21 +2,16 @@ import { FastifyInstance, FastifyRequest } from "fastify";
 import UserController from "../controllers/UserController";
 import { SQLiteUserRepository } from "../repositories/sqlite/SQLiteUserRepository";
 
+const userRepository = new SQLiteUserRepository();
+const userController = new UserController(userRepository);
+
 async function userRoutes(server: FastifyInstance) {
   server.get('/ping/:username', async (request: FastifyRequest<{ Params: { username: string } }>, reply) => {
-    const userRepository = new SQLiteUserRepository();
-    const userController = new UserController(userRepository);
-
     await userController.pingUser(request, reply);
-
-    return 'OK!';
   });
 
-  server.post("/user", (request, reply) => {
-    const userRepository = new SQLiteUserRepository();
-    const userController = new UserController(userRepository);
-    userController.register(request, reply);
-    return "pong\n";
+  server.post("/user", async (request, reply) => {
+    await userController.register(request, reply);
   });
 }
 
