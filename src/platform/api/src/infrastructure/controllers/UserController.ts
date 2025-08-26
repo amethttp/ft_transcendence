2354815099669980236dc4cf152c40ac1bb7f5ac
arzelcm <arzelcanavate@gmail.com>
2025-08-26 17:08:31 +0200

    refactor(platform): delete unnecessary check for user profile endpoint

--- src/platform/api/src/infrastructure/controllers/UserController.ts
+++ src/platform/api/src/infrastructure/controllers/UserController.ts
@@ -23,19 +23,8 @@ export default class UserController {
   }
 
   async pingUser(request: FastifyRequest<{ Params: { username: string } }>, reply: FastifyReply) {
-    const requestedUser = request.user as JwtPayloadInfo;
-
     return this.userService.getUserByUsername(request.params.username)
-      .then(user => {
-        if (requestedUser.sub !== user.id) {
-          return reply.code(403).send({
-            success: false,
-            error: request.params.username + ' says: You are not allowed to read my stuff!',
-          });
-        }
-
-        return reply.code(200).send(user);
-      })
+      .then(user => reply.code(200).send(user))
       .catch(err => reply.code(404).send({
         success: false,
         error: 'The ping echoed nowhere...: ' + err + ': ' + request.params.username,
