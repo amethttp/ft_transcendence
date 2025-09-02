6ee95345e96090cc37fc491b9092fe39973410f2
arzelcm <arzelcanavate@gmail.com>
2025-09-02 20:51:41 +0200

    feat(front): add register component

--- src/front/app/src/PublicLayout/AccessLayout/RegisterComponent/RegisterComponent.ts
+++ src/front/app/src/PublicLayout/AccessLayout/RegisterComponent/RegisterComponent.ts
@@ -1,6 +1,32 @@
+import { LoggedUser } from "../../../auth/LoggedUser";
+import type { RegisterRequest } from "../../../auth/models/RegisterRequest";
+import { AuthService } from "../../../auth/services/AuthService";
 import AmethComponent from "../../../framework/AmethComponent";
 
 export default class RegisterComponent extends AmethComponent {
   template = () => import("./RegisterComponent.html?raw");
-  afterInit() {}
+  private authService!: AuthService;
+  private form!: HTMLFormElement;
+  private errorView!: HTMLElement;
+
+  afterInit() {
+    this.authService = new AuthService();
+    this.form = document.getElementById("registerForm")! as HTMLFormElement;
+    this.errorView = document.getElementById("registerError")!;
+    this.form.onsubmit = e => {
+      e.preventDefault();
+      this.errorView.classList.add("invisible");
+      const registerRequest: RegisterRequest = {
+        username: (this.form[0] as HTMLInputElement).value,
+        email: (this.form[1] as HTMLInputElement).value,
+        password: (this.form[2] as HTMLInputElement).value
+      }
+      this.authService.register(registerRequest)
+        .then(async () => {
+          await LoggedUser.get(true);
+          this.router?.navigateByPath("/home");
+        })
+        .catch(() => this.errorView.classList.remove("invisible"));
+    };
+  }
 }
