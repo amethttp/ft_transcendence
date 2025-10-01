3937b7800edd58a22c75eeb74cf99512c75d52a9
arzelcm <arzelcanavate@gmail.com>
2025-10-01 16:57:48 +0200

    feat(front): add listing actions architecture

--- src/front/app/src/PrivateLayout/PlayComponent/TournamentsListComponent/TournamentsListComponent.ts
+++ src/front/app/src/PrivateLayout/PlayComponent/TournamentsListComponent/TournamentsListComponent.ts
@@ -1,6 +1,19 @@
 import AmethComponent from "../../../framework/AmethComponent";
+import { Form } from "../../../framework/Form/Form";
+import { FormControl } from "../../../framework/Form/FormGroup/FormControl/FormControl";
+import { Validators } from "../../../framework/Form/FormGroup/FormControl/Validators/Validators";
 
 export default class TournamentsListComponent extends AmethComponent {
   template = () => import("./TournamentsListComponent.html?raw");
-  
+  private _form!: Form<{ token: string }>;
+
+  afterInit(): void {
+    this._form = new Form("tournamentCodeListForm", {
+      token: new FormControl<string>("", [Validators.length(1, 30)])
+    });
+
+    this._form.submit = ({ token }) => {
+      this.router?.navigateByPath(`/play/tournament/${token}`);
+    };
+  }
 }
