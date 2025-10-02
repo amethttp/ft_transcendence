b90e82a2d4e8590c1ab04f4c9b960704a6804808
arzelcm <arzelcanavate@gmail.com>
2025-10-02 16:33:10 +0200

    fix(gameapi): listen socket not only on get

--- src/game/api/src/index.ts
+++ src/game/api/src/index.ts
@@ -1,7 +1,7 @@
-import fastify, { type FastifyPluginAsync } from 'fastify';
+import fastify from 'fastify';
 import fs from 'fs';
-import socketPlugin from './plugins/socketPlugin.js';
 import type { Socket } from 'socket.io';
+import fastifySocketIO from 'fastify-socket.io';
 
 const server = fastify({
   https: {
@@ -10,49 +10,63 @@ const server = fastify({
   },
 });
 
-server.register(socketPlugin);
+const main = async () => {
+  await server.register(fastifySocketIO as any, {
+    cors: {
+      origin: `${process.env.CLIENT_HOST}`,
+      credentials: true
+    },
+    transports: ["websocket", "polling"],
+    pingInterval: 10000,
+    pingTimeout: 5000,
+  });
 
-let waitingClient: Socket | null = null;
-server.get("/", (req, reply) => {
-  server.io.on('connection', (socket: Socket) => {
-    console.log(`\nClient: ${socket.id} connected`);
-    socket.on('joinMatch', (token) => {
-      console.log("Trying to join match...", token);
-      if (waitingClient) {
-        const roomId = `match_${token}`;
-        waitingClient.join(roomId);
-        socket.join(roomId);
+  let waitingClient: Socket | null = null;
+  server.ready((err) => {
+    if (err) throw err;
 
-        server.io.to(roomId).emit('handshake', { roomId, message: 'Success??' }); // TODO; emitwithAck??
-        console.log("Players connected successfully:", waitingClient.id, socket.id);
-        waitingClient = null;
-      } else {
-        waitingClient = socket;
-        console.log(`Player ${waitingClient.id} is waiting for a match.`);
-      }
-    });
+    server.io.on('connection', (socket: Socket) => {
+      console.log(`\nClient: ${socket.id} connected`);
+      socket.on('joinMatch', (token) => {
+        console.log("Trying to join match...", token);
+        if (waitingClient) {
+          const roomId = `match_${token}`;
+          waitingClient.join(roomId);
+          socket.join(roomId);
 
-    socket.on('helloWorld', (token) => {
-      socket.broadcast.emit("message", "Hello broadcast");
-      socket.emit("message", "Hello emit");
-      server.io.to(`match_${token}`).emit("message", "server emit");
-    });
+          server.io.to(roomId).emit('handshake', { roomId, message: 'Success??' }); // TODO; emitwithAck??
+          console.log("Players connected successfully:", waitingClient.id, socket.id);
+          waitingClient = null;
+        } else {
+          waitingClient = socket;
+          console.log(`Player ${waitingClient.id} is waiting for a match.`);
+        }
+      });
 
-    socket.on('disconnect', (reason: string) => {
-      console.log(`Client: ${socket.id} disconnected | ${reason}`);
-      if (waitingClient === socket) {
-        waitingClient = null;
-      } else {
-        server.io.emit(`${socket.id} left`);
-      }
+      socket.on('helloWorld', (token) => {
+        socket.broadcast.emit("message", "Hello broadcast");
+        socket.emit("message", "Hello emit");
+        server.io.to(`match_${token}`).emit("message", "server emit");
+      });
+
+      socket.on('disconnect', (reason: string) => {
+        console.log(`Client: ${socket.id} disconnected | ${reason}`);
+        if (waitingClient === socket) {
+          waitingClient = null;
+        } else {
+          server.io.emit(`${socket.id} left`);
+        }
+      });
     });
   });
-});
 
-server.listen({ port: 443, host: '0.0.0.0' }, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Server listening at ${address}`);
-});
+  server.listen({ port: 443, host: '0.0.0.0' }, (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+  });
+}
+
+main();
