e3e40396baed9d062148c8913db0b34bc46faa34
cfareste <chrisfidare@yahoo.com>
2025-10-06 15:08:45 +0200

    feat: add match canvas

--- src/front/app/src/PrivateLayout/PlayComponent/MatchComponent/MatchEngineComponent/MatchEngineComponent.ts
+++ src/front/app/src/PrivateLayout/PlayComponent/MatchComponent/MatchEngineComponent/MatchEngineComponent.ts
@@ -2,10 +2,15 @@ import AmethComponent from "../../../../framework/AmethComponent";
 import type { Router } from "../../../../framework/Router/Router";
 import { io, Socket } from "socket.io-client";
 
-export default class MatchEngineComponent extends AmethComponent {
+export type MatchEngineEvents = {
+  newPlayer: number;
+};
+
+export default class MatchEngineComponent extends AmethComponent<MatchEngineEvents> {
   template = () => import("./MatchEngineComponent.html?raw");
   private _token?: string;
   private _socket: Socket | null;
+  private _canvas!: HTMLCanvasElement;
 
   constructor(token?: string) {
     super();
@@ -18,8 +23,9 @@ export default class MatchEngineComponent extends AmethComponent {
 
     this._socket = io("https://localhost:8081")
   }
-  
+
   afterInit() {
+    // Stone-200 oklch(92.3% 0.003 48.717)
     // this.outlet!.innerHTML = this._token as string;
     this._socket?.on("connect", () => {
       console.log("Connected:", this._socket?.id, this._socket?.connected);
@@ -27,6 +33,7 @@ export default class MatchEngineComponent extends AmethComponent {
     });
     this._socket?.on("handshake", (data) => {
       console.log("Handshake:", data);
+      this.emit('newPlayer', data);
     });
     this._socket?.on("message", (data) => {
       console.log("Message:", data);
@@ -38,6 +45,7 @@ export default class MatchEngineComponent extends AmethComponent {
       this._socket?.emit("helloWorld", this._token);
     };
     document.getElementById("title")!.innerHTML = "ENGINE: " + (this._token as string);
+    this._canvas = document.getElementById('matchCanvas') as HTMLCanvasElement;
   }
 
   async refresh(token?: string) {
