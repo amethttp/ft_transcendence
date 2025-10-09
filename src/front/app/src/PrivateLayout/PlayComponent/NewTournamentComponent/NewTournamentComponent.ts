f62054f3575b5d03e35ea9ecd289c63a5f85fb08
arzelcm <arzelcanavate@gmail.com>
2025-10-09 18:03:51 +0200

    feat(front): add rounds slider

--- src/front/app/src/PrivateLayout/PlayComponent/NewTournamentComponent/NewTournamentComponent.ts
+++ src/front/app/src/PrivateLayout/PlayComponent/NewTournamentComponent/NewTournamentComponent.ts
@@ -3,30 +3,41 @@ import AmethComponent from "../../../framework/AmethComponent";
 import { Form } from "../../../framework/Form/Form";
 import { FormControl } from "../../../framework/Form/FormGroup/FormControl/FormControl";
 import { Validators } from "../../../framework/Form/FormGroup/FormControl/Validators/Validators";
-import type {  NewTournamentRequest } from "./models/NewTournamentRequest";
+import type { Router } from "../../../framework/Router/Router";
+import type { NewTournamentRequest } from "./models/NewTournamentRequest";
+import RoundsSliderComponent from "./RoundsSliderComponent/RoundsSliderComponent";
 import { NewTournamentService } from "./services/NewTournamentService";
 
 export default class NewTournamentComponent extends AmethComponent {
   template = () => import("./NewTournamentComponent.html?raw");
   private _form?: Form<NewTournamentRequest> | null;
   private _service: NewTournamentService;
+  private _roundsSlider: RoundsSliderComponent;
 
   constructor() {
     super();
     this._service = new NewTournamentService();
+    this._roundsSlider = new RoundsSliderComponent("Max. Players");
+
+  }
+
+  async init(selector: string, router?: Router): Promise<void> {
+    await super.init(selector, router);
+    await this._roundsSlider.init("roundsSlider", router);
   }
 
   afterInit(): void {
+    this._roundsSlider.afterInit();
     this._form = new Form("newTournamentForm", {
       // TODO: Generated name
       name: new FormControl<string>("", [Validators.length(3, 100)]),
       points: new FormControl<number>(10, [Validators.minMax(2, 100)]),
-      playersAmount: new FormControl<number>(4, [Validators.minMax(4, 32)]),
+      playersAmount: this._roundsSlider.control,
       isVisible: new FormControl<boolean>(true)
     });
 
     this._form.submit = (val) => {
-      this._service.newTournament(val).then(({token}) => this.router?.navigateByPath(`/play/tournament/${token}`)).catch(err => Alert.error("error", JSON.stringify(err)));
+      this._service.newTournament(val).then(({ token }) => this.router?.navigateByPath(`/play/tournament/${token}`)).catch(err => Alert.error("error", JSON.stringify(err)));
     }
   }
 }
