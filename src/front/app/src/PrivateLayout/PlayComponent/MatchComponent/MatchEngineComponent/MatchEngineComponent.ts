ee57f722fa029b7fc9cd6b7bd5cb4b47c354685b
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2025-10-20 16:01:28 +0200

    feat: add dynamic object painting in canvas

--- src/front/app/src/PrivateLayout/PlayComponent/MatchComponent/MatchEngineComponent/MatchEngineComponent.ts
+++ src/front/app/src/PrivateLayout/PlayComponent/MatchComponent/MatchEngineComponent/MatchEngineComponent.ts
@@ -14,6 +14,11 @@ export default class MatchEngineComponent extends AmethComponent<MatchEngineEven
   private _canvas!: HTMLCanvasElement;
   private _canvasContext!: CanvasRenderingContext2D;
   private _canvasContainer!: HTMLDivElement;
+  private _viewportWidth: number = 1600;
+  private _viewportHeight: number = 900;
+  private _scale!: number;
+  private _offsetX!: number;
+  private _offsetY!: number;
 
   constructor(token?: string) {
     super();
@@ -49,8 +54,8 @@ export default class MatchEngineComponent extends AmethComponent<MatchEngineEven
     this._canvasContainer = document.getElementById('matchCanvasContainer') as HTMLDivElement;
 
     this.resizeCanvas();
-    this.observeResize();
     this.prepareCanvas();
+    this.observeResize();
   }
 
   async refresh(token?: string) {
@@ -68,7 +73,10 @@ export default class MatchEngineComponent extends AmethComponent<MatchEngineEven
   }
 
   private observeResize() {
-    const observer = new ResizeObserver(() => this.resizeCanvas());
+    const observer = new ResizeObserver(() => {
+      this.resizeCanvas();
+      this.prepareCanvas();
+    });
     observer.observe(this._canvasContainer);
   }
 
@@ -99,45 +107,62 @@ export default class MatchEngineComponent extends AmethComponent<MatchEngineEven
 
     this._canvas.style.left = `${(containerWidth - cssCanvasWidth) / 2}px`;
     this._canvas.style.top = `${(containerHeight - cssCanvasHeight) / 2}px`;
+
+    const logicalScaleCSS = Math.min(cssCanvasWidth / this._viewportWidth, cssCanvasHeight / this._viewportHeight);
+    const finalScale = logicalScaleCSS * devicePixelRatio;
+
+    const offsetXCSS = (cssCanvasWidth - this._viewportWidth * logicalScaleCSS) / 2;
+    const offsetYCSS = (cssCanvasHeight - this._viewportHeight * logicalScaleCSS) / 2;
+
+    const offsetXPhysical = offsetXCSS * devicePixelRatio;
+    const offsetYPhysical = offsetYCSS * devicePixelRatio;
+
+    this._canvasContext.setTransform(finalScale, 0, 0, finalScale, offsetXPhysical, offsetYPhysical);
+
+    this._scale = logicalScaleCSS;
+    this._offsetX = offsetXCSS;
+    this._offsetY = offsetYCSS;
   }
   
   private prepareCanvas() {
-    let color = 'oklch(97% 0.001 106.424)';
-    let paddleWidth = 10;
-    let paddleHeight = 20;
+    this._canvasContext.save();
 
-    let paddle1 = {
-        x : 10,
-        y : this._canvas.height/2 - paddleHeight / 2,
-        width: paddleWidth,
-        height: paddleHeight,
-        velocityY : 0
-    }
+    const paddleWidth = this._viewportWidth * 0.01;
+    const paddleHeight = this._viewportHeight * 0.15;
+    const ballSize = this._viewportWidth * 0.01;
 
-    let paddle2 = {
-        x : this._canvas.width/2 - paddleWidth - 10,
-        y : this._canvas.height/4 - paddleHeight / 2,
-        width: paddleWidth,
-        height: paddleHeight,
-        velocityY : 0
-    }
+    const paddle1 = {
+      x: 100,
+      y: this._viewportHeight / 2 - paddleHeight / 2,
+      width: paddleWidth,
+      height: paddleHeight,
+      velocityY: 0
+    };
 
-    //ball
-    let ballDimension = 50;
-    let ball = {
-        x : this._canvas.width/4 - ballDimension / 2,
-        y : this._canvas.height/4 - ballDimension / 2,
-        width: ballDimension,
-        height: ballDimension,
-        velocityX : 1,
-        velocityY : 2
-    }
+    const paddle2 = {
+      x: this._viewportWidth - paddleWidth - 100,
+      y: this._viewportHeight / 2 - paddleHeight / 2,
+      width: paddleWidth,
+      height: paddleHeight,
+      velocityY: 0
+    };
 
-    this._canvasContext.fillStyle = color;
+    const ball = {
+      x: this._viewportWidth / 2 - ballSize / 2,
+      y: this._viewportHeight / 2 - ballSize / 2,
+      width: ballSize,
+      height: ballSize,
+      velocityX: 1,
+      velocityY: 2
+    };
+
+    this._canvasContext.fillStyle = 'oklch(21.6% 0.006 56.043)';
+    this._canvasContext.fillRect(0, 0, 1600, 900);
+    this._canvasContext.fillStyle = "oklch(97% 0.001 106.424)";
     this._canvasContext.fillRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
     this._canvasContext.fillRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
     this._canvasContext.fillRect(ball.x, ball.y, ball.width, ball.height);
 
-    // BUG WITH requestPrepareAnimation(this.update) DOESNT WORK WITH "this", this is undefined
+    this._canvasContext.restore();
   }
 }
