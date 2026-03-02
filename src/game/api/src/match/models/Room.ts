0fc1d092ac1cdf422827db6bd3102411f63311e8
arzelcm <arzelcanavate@gmail.com>
2026-03-02 17:46:40 +0100

    feat: emit handshake event for players only when not paused

--- src/game/api/src/match/models/Room.ts
+++ src/game/api/src/match/models/Room.ts
@@ -179,10 +179,12 @@ export class Room extends EventEmitter<RoomEvents> {
 
     this.addHumanPlayer(socket);
     socket.broadcast.to(this.token).emit("message", `New Opponent: ${socket.username}(${socket.id}`);
-    socket.broadcast.to(this.token).emit("handshake", socket.userId);
     if (this._matchState === MatchState.PAUSED) {
       socket.broadcast.to(this.token).emit("reset", socket.userId);
       this.resetPlayersState();
+    }
+    else {
+      socket.broadcast.to(this.token).emit("handshake", socket.userId);
     }
   }
 
